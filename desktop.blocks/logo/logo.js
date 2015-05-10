91cd973a07d0d245ac20352955481bdf6a5d2551
andychups <andychups@ya.ru>
2015-05-11 00:00:55 +0300

    Слайд вверх

--- desktop.blocks/logo/logo.js
+++ desktop.blocks/logo/logo.js
@@ -8,9 +8,16 @@ modules.define('logo', ['i-bem__dom'], function (provide, BEMDOM) {
                     'inited': function () {
                         var block = this;
                         var $window = $(window);
+                        var $htmlAndBody = $('html, body');
 
                         $window.on('scroll', function () {
                             block.checkSize($(this).scrollTop());
+                        });
+
+                        this.bindTo('click', function (e) {
+                            $htmlAndBody.animate({'scrollTop': 0}, 500);
+
+                            e.preventDefault();
                         });
 
                         this.checkSize($window.scrollTop());
